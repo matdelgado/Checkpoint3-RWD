b270abec69baf1c6dcab54647af1ca5ae6d98b26
matdelgado <matdelgado0@gmail.com>
2023-05-20 20:18:30 -0300

    Adicionando cores nas verificações do formulário da questão 1.

--- js/cp3.js
+++ js/cp3.js
@@ -1,4 +1,37 @@
+const inputFirstName = document.querySelector("#idFirstName");
+
+inputFirstName.addEventListener("keyup", ()=>{
+
+    const lblFirstName = document.querySelector("label[for='idFirstName']");
+
+    if (inputFirstName.value.length < 5){
+        inputFirstName.setAttribute("style", "outline-color:#ff0000;");
+        lblFirstName.setAttribute("style", "color:#ff0000;");
+    } else {
+        inputFirstName.setAttribute("style", "outline-color:#00ff00;");
+        lblFirstName.setAttribute("style", "color:#00ff00;");
+    }
+
+});
+
+const inputLastName = document.querySelector("#idLastName");
+
+inputLastName.addEventListener("keyup", ()=>{
+
+    const lblLastName = document.querySelector("label[for='idLastName']");
+
+    if (inputLastName.value.length < 5){
+        inputLastName.setAttribute("style", "outline-color:#ff0000;");
+        lblLastName.setAttribute("style", "color:#ff0000;");
+    } else {
+        inputLastName.setAttribute("style", "outline-color:#00ff00;");
+        lblLastName.setAttribute("style", "color:#00ff00;");
+    }
+
+});
+
 const inputEmail = document.querySelector("#idEmail");
+
 
 inputEmail.addEventListener("keyup", ()=>{
 
@@ -11,5 +44,39 @@ inputEmail.addEventListener("keyup", ()=>{
         inputEmail.setAttribute("style", "outline-color:#00ff00;");
         lblEmail.setAttribute("style", "color:#00ff00;");
     }
+
+});
+
+const inputPass = document.querySelector("#idPass");
+
+inputPass.addEventListener("keyup", ()=>{
+
+    const lblPass = document.querySelector("label[for='idPass']");
+
+    if (inputPass.value.length < 5){
+        inputPass.setAttribute("style", "outline-color:#ff0000;");
+        lblPass.setAttribute("style", "color:#ff0000;");
+    } else {
+        inputPass.setAttribute("style", "outline-color:#00ff00;");
+        lblPass.setAttribute("style", "color:#00ff00;");
+    }
+
+
+});
+
+const inputConf = document.querySelector("#idConfPass");
+
+inputConf.addEventListener("keyup", ()=>{
+
+    const lblConf = document.querySelector("label[for='idConfPass']");
+
+    if (inputConf.value != inputPass.value){
+        inputConf.setAttribute("style", "outline-color:#ff0000;");
+        lblConf.setAttribute("style", "color:#ff0000;");
+    } else {
+        inputConf.setAttribute("style", "outline-color:#00ff00;");
+        lblConf.setAttribute("style", "color:#00ff00;");
+    }
+
 
 });
